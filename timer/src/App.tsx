fdbf00801255b135e36e1aef9145f66cf5aba22d
kleberlimagit <mov_kleber@hotmail.com>
2022-12-31 10:29:58 -0300

    React Router Dom

--- timer/src/App.tsx
+++ timer/src/App.tsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
-import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/themes/global'
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/themes/global";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant='primary'/>
-      <Button variant='secondary'/>
-      <Button variant='danger'/>
-      <Button/>
-      <GlobalStyle/>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
-  )
+  );
 }
