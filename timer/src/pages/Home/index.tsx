b00166305500966b9ddc252af0440dfa11a2e4aa
kleberlimagit <mov_kleber@hotmail.com>
2023-01-04 10:39:30 -0300

    Zod e validaçoes de erro

--- timer/src/pages/Home/index.tsx
+++ timer/src/pages/Home/index.tsx
@@ -9,23 +9,35 @@ import {
   TaskInput,
 } from "./styles";
 
-import { useForm } from 'react-hook-form' 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
 
 // controlled -> mantém em tempo real a informaçao do estado do input
 
 //uncontrolled -> busca informaçao do valor do input somente quando precisarmos dela
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "Valor mínimo deve ser 5 minutos")
+    .max(60, "Valor máximo deve ser 60 minutos")
+});
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch, formState } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  });
 
-
-  function handleCreateNewCycle(data: any){
-    console.log(data)
+  function handleCreateNewCycle(data: any) {
+    console.log(data);
   }
 
-  const task = watch('task')
-  const minutesAmount = watch('minutesAmount')
-  const isSubmitDisabled = (!task && !minutesAmount)
+  const task = watch("task");
+  const isSubmitDisabled = !task;
+
+console.log(formState.errors)
 
   return (
     <HomeContainer>
@@ -36,7 +48,7 @@ export function Home() {
             type="text"
             id="task"
             placeholder="Dê um nome para o seu projeto"
-            {...register('task')}
+            {...register("task")}
           />
 
           <label htmlFor="minutesAmount">durante</label>
@@ -48,7 +60,7 @@ export function Home() {
             min={5}
             max={60}
             required
-            {...register('minutesAmount', {valueAsNumber:true})}
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
