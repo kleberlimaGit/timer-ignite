76ec4d74b3a53c64470147d809781b4660b396aa
kleberlimagit <mov_kleber@hotmail.com>
2023-01-08 21:35:21 -0300

    dados imutaveis com a biblioteca immer

--- timer/src/reducers/cycles/reducer.ts
+++ timer/src/reducers/cycles/reducer.ts
@@ -1,55 +1,58 @@
+import { produce } from "immer";
+
 import { ActionTypes } from "./actions";
 
 interface CyclesState {
-    cycles: Cycle[];
-    activeCycleId: string | null;
-  }
+  cycles: Cycle[];
+  activeCycleId: string | null;
+}
 
-  export interface Cycle {
-    id: string;
-    task: string;
-    minutesAmount: number;
-    startDate: Date;
-    interruptedDate?: Date;
-    finishedDate?: Date;
-  }
+export interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+  startDate: Date;
+  interruptedDate?: Date;
+  finishedDate?: Date;
+}
 
+export function cyclesReducer(state: CyclesState, action: any) {
+  switch (action.type) {
+    case ActionTypes.ADD_NEW_CYCLE:
+      return produce(state, (draft) => {
+        draft.cycles.push(action.payload.newCycle);
+        draft.activeCycleId = action.payload.newCycle.id;
+      });
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId;
+      });
 
-
-export function cyclesReducer(state: CyclesState, action: any){
-    switch (action.type) {
-      case ActionTypes.ADD_NEW_CYCLE:
-        return {
-          ...state,
-          cycles: [...state.cycles, action.payload.newCycle],
-          activeCycleId: action.payload.newCycle.id,
-        };
-      case ActionTypes.INTERRUPT_CURRENT_CYCLE:
-        return {
-          ...state,
-          cycles: state.cycles.map((cycle) => {
-            if (state.activeCycleId === cycle.id) {
-              return { ...cycle, interruptedDate: new Date() };
-            } else {
-              return cycle;
-            }
-          }),
-          activeCycleId: null,
-        };
-
-      case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISH:
-        return {
-          ...state,
-          cycles: state.cycles.map((cycle) => {
-            if (state.activeCycleId === cycle.id) {
-              return { ...cycle, finishedDate: new Date() };
-            } else {
-              return cycle;
-            }
-          }),
-          activeCycleId: null,
-        };
-      default:
+      if (currentCycleIndex < 0) {
         return state;
+      }
+
+      return produce(state, (draft) => {
+        draft.activeCycleId = null;
+        draft.cycles[currentCycleIndex].interruptedDate = new Date();
+      });
     }
+
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISH:{
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId;
+      });
+
+      if (currentCycleIndex < 0) {
+        return state;
+      }
+
+      return produce(state, (draft) => {
+        draft.activeCycleId = null;
+        draft.cycles[currentCycleIndex].finishedDate = new Date();
+      });
+    }
+    default:
+      return state;
   }
+}
